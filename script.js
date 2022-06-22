3a869e9b5b03f1a88bdf7481e6f69abd53c4e2e0
mrrpablo <precijagz480@gmail.com>
2022-06-22 16:51:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.write('<h1>Introduction to Javascript</h1>');
+document.write('<h2>Name: Precious Asuquo Bassey</h2>');
+document.write('<h4>Height: 5.6 feet</h4>');
+document.write('<h4>Country: Nigeria</h4>');
